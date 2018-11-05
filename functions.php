603c3650a9f7c3c87d7030afda758246803e24dc
Max le Fou <buttbadger@gmail.com>
2018-11-05 12:52:32 +0100

    updated fontawesome again

--- functions.php
+++ functions.php
@@ -177,7 +177,7 @@ function duena_styles() {
 	wp_enqueue_style( 'magnific' );
 
 	// FontAwesome stylesheet
-	wp_register_style( 'font-awesome', '//use.fontawesome.com/releases/v5.4.2/css/all.css', '', '5.4.2');
+	wp_register_style( 'font-awesome', '//use.fontawesome.com/releases/v5.5.0/css/all.css', '', '5.5.0');
 	wp_enqueue_style( 'font-awesome' );
 
 	// Main stylesheet
