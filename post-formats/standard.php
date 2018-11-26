07c49a14f283ea00fe3fe83634b3a242fd55b508
Max le Fou <buttbadger@gmail.com>
2018-11-26 17:31:53 +0100

    2.1.0

--- post-formats/standard.php
+++ post-formats/standard.php
@@ -2,30 +2,31 @@
 
 				<?php
 					$stickyclass = 'sticky';
-					get_template_part('post-formats/post-thumb'); 
+					get_template_part('post-formats/post-thumb');
 				?>
 
 				<header class="post-header <?php if( is_singular() && is_sticky() ) echo esc_attr( $stickyclass ); ?>">
-					<?php if ( is_sticky() ) echo "<span class='featured_badge'><i class='fa fa-thumb-tack'></i><strong>".__( 'Featured', 'duena' )."</strong></span>"; ?>
+					<?php if ( is_sticky() ) echo "<span class='featured_badge'><i class='fas fa-thumbtack' style='font-weight: 900;'></i><strong>".__( 'Featured', 'duena' )."</strong></span>";
+					// Dear FontAwesome WHY MUST I PUT THAT FONT WEIGHT STUFF MYSELF HERE ?!!!!! ?>
 					<?php if(!is_singular()) : ?>
-					
+
 					<h3 class="post-title"><a href="<?php the_permalink(); ?>" title="<?php esc_attr_e('Permalink to:', 'duena');?> <?php the_title(); ?>"><?php the_title(); ?></a></h3>
-					
+
 					<?php else :?>
-					
+
 					<h1 class="post-title"><?php the_title(); ?></h1>
-					
+
 					<?php endif; ?>
-				
+
 				</header>
-				
+
 				<?php if(!is_singular()) : ?>
-				
+
 				<!-- Post Content -->
 				<div class="post_content">
 					<?php if ( 'false' != of_get_option('post_excerpt')) { ?>
 						<div class="excerpt">
-						<?php 
+						<?php
 							$excerpt = get_the_excerpt();
 							if (has_excerpt()) {
 									the_excerpt();
@@ -35,23 +36,23 @@
 						?>
 						</div>
 					<?php } ?>
-					<?php 
-						if ( 'false' != of_get_option('post_button')) { 
+					<?php
+						if ( 'false' != of_get_option('post_button')) {
 							$button_text = of_get_option( 'post_button_txt' );
 							if ( '' == $button_text ) $button_text = __( 'Read more', 'duena' );
 					?>
 						<a href="<?php the_permalink() ?>" class="more_link"><?php echo $button_text; ?></a>
 					<?php } ?>
 				</div>
-				
+
 				<?php else :?>
-				
+
 				<!-- Post Content -->
 				<div class="post_content">
-				
+
 					<?php the_content(''); ?>
-					<?php 
-						wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'duena' ), 'after' => '</div>', 'link_before' => '<span>', 'link_after' => '</span>' ) ); 
+					<?php
+						wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'duena' ), 'after' => '</div>', 'link_before' => '<span>', 'link_after' => '</span>' ) );
 					?>
 				</div>
 				<!-- //Post Content -->
@@ -61,7 +62,7 @@
 				</footer>
 				<?php } ?>
 				<?php endif; ?>
-				
+
 				<?php get_template_part('post-formats/post-meta'); ?>
 
 			</article>
