6c3743061fc12a7886a3a8c7733169c606f00d5a
maxlefou <buttbadger@gmail.com>
2021-11-05 14:39:39 +0100

    5.0.0

--- inc/extras.php
+++ inc/extras.php
@@ -45,31 +45,6 @@ function duena_revival_enhanced_image_navigation( $url, $id ) {
 add_filter( 'attachment_link', 'duena_revival_enhanced_image_navigation', 10, 2 );
 
 /**
- * Filters wp_title to print a neat <title> tag based on what is being viewed.
- */
-function duena_revival_wp_title( $title, $sep ) {
-	global $page, $paged;
-
-	if ( is_feed() )
-		return $title;
-
-	// Add the blog name
-	$title .= get_bloginfo( 'name' );
-
-	// Add the blog description for the home/front page.
-	$site_description = get_bloginfo( 'description', 'display' );
-	if ( $site_description && ( is_home() || is_front_page() ) )
-		$title .= " $sep $site_description";
-
-	// Add a page number if necessary:
-	if ( $paged >= 2 || $page >= 2 )
-		$title .= " $sep " . sprintf( __( 'Page %s', 'duena-revival' ), max( $paged, $page ) );
-
-	return $title;
-}
-add_filter( 'wp_title', 'duena_revival_wp_title', 10, 2 );
-
-/**
  * Show gallery on blog page
  */
 function duena_revival_gallery_sl() {
